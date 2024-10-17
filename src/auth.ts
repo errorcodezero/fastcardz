64ad6945d3d7e1cca860223a545c76d8626c0520
ErrorCode0 <me@errorcodezero.dev>
2024-10-17 09:42:05 -0700

    Adding auth.js

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,9 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import { DrizzleAdapter } from '@auth/drizzle-adapter';
 import { db } from '$lib/db';
+import Google from "@auth/sveltekit/providers/google"
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
 	adapter: DrizzleAdapter(db),
-	providers: []
+	providers: [Google]
 });
---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { Button } from "m3-svelte"
+</script>
+
+<Button type="tonal"> 
+	Google
+</Button>
