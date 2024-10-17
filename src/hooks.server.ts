9b4ea84be4f3ba2f4ba9549d795a4289385001cb
ErrorCode0 <me@errorcodezero.dev>
2024-10-17 11:14:05 -0700

    Adding trpc

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,1 +1,9 @@
-export { handle } from './auth';
+import { createContext } from '$lib/trpc/context';
+import { router } from '$lib/trpc/router';
+import type { Handle } from '@sveltejs/kit';
+import { sequence } from '@sveltejs/kit/hooks';
+import { createTRPCHandle } from 'trpc-sveltekit';
+import { handle as authHandle } from './auth';
+const trpcHandle: Handle = createTRPCHandle({ router, createContext });
+
+export const handle = sequence(authHandle, trpcHandle); 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
 	import { Button } from 'm3-svelte';
+	import { trpc } from "$lib/trpc/client"
+	import { page } from '$app/stores';
+
+	let greeting = $state("press button to do smth")
+
+	const loadData = async () => {
+		greeting = await trpc($page).greeting.query();
+	}
 </script>
 
 <div class="grid place-items-center space-y-2">
 	<h1 class="text-4xl">Flashcards have never been this good</h1>
-	<Button type="tonal">Let's go</Button>
+	<Button type="tonal" on:click={async () => loadData()}>Let's go</Button>
+	<p>{greeting}</p>
 </div>
